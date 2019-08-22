bd2c125bd846b73b6c9d7bae37150b135f9df6bf
vetkan <47370101+vetkan@users.noreply.github.com>
2019-08-22 14:54:25 -0500

    Add files via upload

--- Widget Code.py
+++ Widget Code.py
@@ -1,9 +1,41 @@
 from tkinter import *
 
+#We need to iport the code for backend functions which comes from another .py file
+
+import backend
+
+def view_command():
+    list1.delete(0,END) #deletes from row of index = 0 to end
+    for row in backend.view():
+        list1.insert(END, row) #new rows will be put at the end
+        
+#the current ind is a  string var object and we need to get it. Hence we use .get
+
+def search_command(Row_Current_Ind.get(),Row_Deleted_Ind.get(),CPT_Codes.get(),Valid_From.get(),Valid_To.get(),Key_Value.get()):
+    list1.delete(0,END)
+    for row in backend.search(Row_Current_Ind.get(),Row_Deleted_Ind.get(),CPT_Codes.get(),Valid_From.get(),Valid_To.get(),Key_Value.get()):
+        list1.insert(End,row)
+
+        
+def add_command():
+    backend.insert(Row_Current_Ind.get(),Row_Deleted_Ind.get(),CPT_Codes.get(),Valid_From.get(),Valid_To.get(),Key_Value.get())
+    list1.delete(0.END)
+    list1.insert(END,(Row_Current_Ind.get(),Row_Deleted_Ind.get(),CPT_Codes.get(),Valid_From.get(),Valid_To.get(),Key_Value.get()))
+    
+def
+    
+        
+        
 window = Tk()
+window.title("Star Tool")
 
 l1 = Label(window,text = "Row Current Ind")
 l1.grid(row=1,column=0)
+
+
+#row and column define the position of the lable within the grid.
+#The grid command is used to position the desired field, i.e. either a lable, or a button or listbox etc.
+
 
 l2 = Label(window,text = "Row Deleted Ind")
 l2.grid(row=1,column=2)
@@ -51,6 +83,12 @@ Key_Value=IntVar()
 e6=Entry(window,textvariable=Key_Value)
 e6.grid(row=3,column=1)
 
+
+#List box is used to create a box that displays all the needd values inserted
+#height and width are length and breath of the box. They are not the same size as row and column
+#rowspan and column span are used to define how many fields the box can reach
+
+
 list1=Listbox(window, height = 15, width = 135)
 list1.grid(row=4,column=0,rowspan =6, columnspan=4)
 
@@ -60,13 +98,13 @@ sb1.grid(row =4,column =4,rowspan = 6)
 list1.configure(yscrollcommand = sb1.set)
 sb1.configure(command=list1.yview)
 
-b1=Button(window,text="View All", width=12)
+b1=Button(window,text="View All", width=12, command=view_command)
 b1.grid(row=4,column = 5)
 
-b2=Button(window,text="Search Entry", width=12)
+b2=Button(window,text="Search Entry", width=12, command=search_command)
 b2.grid(row=5,column = 5)
 
-b3=Button(window,text="Add Entry", width=12)
+b3=Button(window,text="Add Entry", width=12, command=add_command)
 b3.grid(row=6,column = 5)
 
 b4=Button(window,text="Update Selected", width=12)
